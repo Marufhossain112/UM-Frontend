27750d5d06d7a15d588959cc90b4b85647540887
Maruf Hossain <dev.maruf112@gmail.com>
2024-04-24 16:32:45 +0600

    dashboard code distributed

---
+++ src/components/ui/Content.tsx
@@ -0,0 +1,15 @@
+import { Content } from 'antd/es/layout/layout';
+import React from 'react';
+
+const Contents = ({ children }: { children: React.ReactNode; }) => {
+
+    return (
+        <Content style={{ minHeight: "100vh" }}>
+            {
+                children
+            }
+        </Content>
+    );
+};
+
+export default Contents;
