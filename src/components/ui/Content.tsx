903900d9fab58741c8eb111b1d46edff1c277f3e
Maruf Hossain <dev.maruf112@gmail.com>
2024-04-26 21:02:00 +0600

    breadcrumb added

--- src/components/ui/Content.tsx
+++ src/components/ui/Content.tsx
@@ -1,10 +1,23 @@
 import { Content } from 'antd/es/layout/layout';
 import React from 'react';
+import UMBreadCrumb from './UMBreadCrumb';
 
 const Contents = ({ children }: { children: React.ReactNode; }) => {
-
+    const base = 'admin';
     return (
         <Content style={{ minHeight: "100vh" }}>
+            <UMBreadCrumb
+                items={[
+                    {
+                        label: `${base}`,
+                        link: `/${base}`,
+                    },
+                    {
+                        label: "student",
+                        link: `/${base}/student`,
+                    },
+                ]}
+            />
             {
                 children
             }
